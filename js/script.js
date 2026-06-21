d3e55db7df34258819e9feeeb13d3cf5ce8ab885
Abanoub Nashat <nashatabanop@gmail.com>
2026-06-21 19:47:22 +0300

    add show details display functionality and remove placeholder content

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ async function displayPopularMovies() {
               class="card-img-top"
               alt="Movie Title"
             />` : `<img
-            src"../images/no-image.jpg"
+            src="../images/no-image.jpg"
             class="card-img-top"
             alt="Movie Title"
             >` }
@@ -71,13 +71,13 @@ async function displayPopularShows() {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-          <a href="movie-details.html?id=${show.id}">
+          <a href="tv-details.html?id=${show.id}">
           ${show.poster_path ? `  <img
               src="https://image.tmdb.org/t/p/w500${show.poster_path}"
               class="card-img-top"
               alt="Movie Title"
             />` : `<img
-            src"../images/no-image.jpg"
+            src="../images/no-image.jpg"
             class="card-img-top"
             alt="Movie Title"
             >` }
@@ -108,7 +108,7 @@ async function displayMovieDetails() {
               class="card-img-top"
               alt="${movie.title}"
             />` : `<img
-            src"../images/no-image.jpg"
+            src="../images/no-image.jpg"
             class="card-img-top"
             alt="${movie.title}"
             >` }
@@ -142,6 +142,57 @@ async function displayMovieDetails() {
           <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
         </div>`
   document.querySelector('#movie-details').appendChild(div);
+}
+
+// display show details
+async function displayShowDetails() {
+  // returns the value after "?"
+  const showId = window.location.search.split('=')[1];
+  const endpoint = `tv/${showId}`;
+  const show = await fetchData(endpoint);
+  console.log(show)
+  displayBackground('show', show.backdrop_path);
+  const div = document.createElement('div');
+  div.innerHTML = ` <div class="details-top">
+          <div>
+          ${show.poster_path ? `  <img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />` : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+            >` }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${show.first_air_date}</p>
+            <p>
+            ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+            <li><span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.name}</li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
+        </div>`
+  document.querySelector('#show-details').appendChild(div);
 }
 
 function addCommasToNumber(number) {
@@ -190,6 +241,7 @@ function init() {
       displayPopularShows();
       break;
     case '/tv-details.html':
+      displayShowDetails();
       console.log('tv show details');
       break;
   }
