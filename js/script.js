ced4c7e7c204d8557782910558acfc7bbf9ee65e
Abanoub Nashat <nashatabanop@gmail.com>
2026-06-21 19:59:14 +0300

    add swiper functionality to display now playing movies

--- js/script.js
+++ js/script.js
@@ -144,6 +144,50 @@ async function displayMovieDetails() {
   document.querySelector('#movie-details').appendChild(div);
 }
 
+// shows the sliding movies
+async function displaySwiper() {
+  const endpoint = 'movie/now_playing';
+  const { results } = await fetchData(endpoint);
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide')
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt= "${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)} / 10
+            </h4>`;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+  })
+  initSwiper();
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2
+      },
+      700: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  })
+}
+
+
 // display show details
 async function displayShowDetails() {
   // returns the value after "?"
@@ -227,6 +271,7 @@ function init() {
     case '/':
     case '/index.html':
       console.log('home');
+      displaySwiper();
       displayPopularMovies();
       break;
     case '/movie-details.html':
