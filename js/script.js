22da05b0960576264e8895f268b2711e0b71fbb8
Abanoub Nashat <nashatabanop@gmail.com>
2026-06-21 18:54:49 +0300

    add background display functionality for movie details

--- js/script.js
+++ js/script.js
@@ -99,6 +99,7 @@ async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
   const endpoint = `movie/${movieId}`;
   const movie = await fetchData(endpoint);
+  displayBackground('movie', movie.backdrop_path);
   const div = document.createElement('div');
   div.innerHTML = ` <div class="details-top">
           <div>
@@ -146,6 +147,27 @@ async function displayMovieDetails() {
 function addCommasToNumber(number) {
   const formatter = new Intl.NumberFormat(`en-US`);
   return formatter.format(number);
+}
+
+function displayBackground(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  }
+  else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 // init resources on app loading
